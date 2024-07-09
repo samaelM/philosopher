6be76722c08fc6f8284b33ac4a7b85e9f29b724b
Mael Maldonado <maemaldo@student.42.fr>
2024-07-09 16:18:47 +0200

    fixed the mixing threads problem

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: maemaldo <maemaldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:59:36 by maemaldo          #+#    #+#             */
-/*   Updated: 2024/07/08 17:26:42 by maemaldo         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:18:07 by maemaldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,25 +46,19 @@ int	ft_eat(t_data *data, int idx)
 	right = idx + 1;
 	if (idx == data->nb_philo - 1)
 		right = 0;
-	// printf("%d ask fork %d\n", idx, left);
 	pthread_mutex_lock(&data->tab_fork[left]);
 	printf("%ld %d has taken a fork %d\n", ft_get_time_ms(data), idx, left);
-	// printf("%d ask fork %d\n", idx, right);
 	pthread_mutex_lock(&data->tab_fork[right]);
 	printf("%ld %d has taken a fork %d\n", ft_get_time_ms(data), idx, right);
 	gettimeofday(&data->time, NULL);
 	data->tab_philo[idx]->last_meal_ms = data->time.tv_sec * 1000
 		+ data->time.tv_usec / 1000;
 	data->tab_philo[idx]->nb_meal++;
-	// data->tab_philo[idx]->last_meal_us = data->time.tv_usec;
 	printf("%ld %d is eating with fork %d and %d\n", data->time.tv_sec * 1000
 		+ data->time.tv_usec / 1000, idx, left, right);
 	usleep(data->time_eat);
 	pthread_mutex_unlock(&data->tab_fork[left]);
-	// printf("%d left fork %d\n", idx, left);
 	pthread_mutex_unlock(&data->tab_fork[right]);
-	// printf("%d left fork %d\n", idx, right);
-	// printf("%d is no longer eating\n", idx);
 	return (1);
 }
 
@@ -99,6 +93,8 @@ t_data	*ft_init(int ac, char **av)
 	data = malloc(sizeof(t_data));
 	if (ac == 6)
 		data->nb_eat_max = ft_atoi(av[5]);
+	else
+		data->nb_eat_max = -1;
 	data->nb_philo = ft_atoi(av[1]);
 	data->time_die = ft_atoi(av[2]) * 1000;
 	data->time_eat = ft_atoi(av[3]) * 1000;
@@ -127,30 +123,24 @@ void	*ft_philo(void *arg)
 	data = info->data;
 	philo = data->tab_philo[info->idx];
 	philo->id = info->idx;
-	philo->nb_meal = 0;
-	// ft_put_pthread(philo->thid);
-	// printf("\n");
-	if ((ret = (char *)malloc(20)) == NULL)
-	{
-		perror("malloc() error");
-		exit(2);
-	}
+	free(info);
 	ft_wait_start(&data->is_started);
 	while (data->is_started)
 	{
 		printf("%ld %d is thinking\n", ft_get_time_ms(data), philo->id);
 		ft_eat(data, philo->id);
-		printf("%d has eaten %d time (max=%d)\n", philo->id, philo->nb_meal, data->nb_eat_max);
+		printf("%d has eaten %d time (max=%d)\n", philo->id, philo->nb_meal,
+			data->nb_eat_max);
 		printf("%d\n", data->nb_eat_max == philo->nb_meal);
 		if (data->nb_eat_max == philo->nb_meal)
 		{
-			printf("BREAAAAKKKKKK %d\n", philo->id);
+			printf("break id %d\n", philo->id);
 			break ;
 		}
 		printf("%ld %d is sleeping\n", ft_get_time_ms(data), philo->id);
 		usleep(data->time_sleep);
 	}
-	strcpy(ret, "This is a test");
+	ret = ft_itoa(philo->id);
 	pthread_exit(ret);
 }
 
@@ -159,37 +149,33 @@ int	main(int ac, char **av)
 	t_data	*data;
 	int		i;
 	void	*ret;
-	t_ti	info;
-	long	a;
-	long	b;
+	t_ti	*info;
 
 	data = ft_init(ac, av);
 	if (!data)
 		return (1);
 	i = 0;
 	printf("time = %ld\n", data->time.tv_usec);
-	info.data = data;
 	while (i < data->nb_philo)
 	{
 		data->tab_philo[i] = malloc(sizeof(t_philo));
-		info.idx = i;
+		data->tab_philo[i]->nb_meal = 0;
+		info = malloc(sizeof(t_ti));
+		info->data = data;
+		info->idx = i;
 		if (pthread_create(&data->tab_philo[i]->thid, NULL, ft_philo,
-				&info) != 0)
+				info) != 0)
 		{
 			perror("pthread_create() error");
 			exit(1);
 		}
-		// usleep(10);
 		i++;
 	}
 	gettimeofday(&data->time, NULL);
-	a = data->time.tv_usec;
-	gettimeofday(&data->time, NULL);
-	b = data->time.tv_usec;
-	printf("time = %ld - %ld\n", a, b);
 	i = 0;
 	data->is_started = 1;
-	printf("----------------start--------------\n"); // doit ajouter une boucle qui verifie que les philo soit pas mort de faim
+	printf("----------------start--------------\n");
+		// doit ajouter une boucle qui verifie que les philo soit pas mort de faim
 	while (i < data->nb_philo)
 	{
 		if (pthread_join(data->tab_philo[i]->thid, &ret) != 0)
